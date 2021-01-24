d71997450e186fcba96ae00ae8e2a3fa5bde8e99
Saugat Rai <saugatrai33@gmail.com>
2021-01-24 16:14:02 +0545

    ui for skills view added.

---
+++ app/src/main/java/com/example/resumeforitpro/ui/home/skill/SkillActivity.kt
@@ -0,0 +1,13 @@
+package com.example.resumeforitpro.ui.home.skill
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.resumeforitpro.R
+
+class SkillActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_skill)
+
+    }
+}
